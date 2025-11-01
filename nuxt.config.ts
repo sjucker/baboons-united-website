db309a7ccf91c1bb18b56cf41520240421b55b60
Stefan Jucker <stefan.jucker@gmail.com>
2025-11-01 13:32:32 +0100

    Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2025-07-15',
-    devtools: {enabled: true},
+  compatibilityDate: '2025-07-15',
+  devtools: {enabled: true},
 
-    modules: [
-        '@nuxt/content',
-        '@nuxt/eslint',
-        '@nuxt/image',
-        '@nuxt/ui'
-    ]
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui'
+  ],
+  image: {},
+  app: {
+    head: {
+      title: 'Baboons United Hedingen',
+      htmlAttrs: {
+        lang: 'de',
+      },
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/favicon/site.webmanifest' },
+      ],
+    }
+  }
 })
