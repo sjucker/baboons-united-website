2f727a26e488228557e866626fa0bde8b0f0a850
Stefan Jucker <stefan.jucker@gmail.com>
2026-01-04 15:53:12 +0100

    WIP: Consume swissunihockey API

--- netlify/functions/games.ts
+++ netlify/functions/games.ts
@@ -1,5 +1,27 @@
 import type {Handler} from '@netlify/functions'
-import type {Game, GamesOverview, SuResponse} from "../../shared/types";
+import type {Game, GameDetails, GamesOverview, SuGameDetailsResponse, SuResponse} from "../../shared/types";
+
+async function fetchGame(id: number): Promise<GameDetails | null> {
+  const url = 'https://api-v2.swissunihockey.ch/api/games/' + id
+  const res = await fetch(url)
+  if (!res.ok) {
+    return null
+  }
+
+  const data = (await res.json()) as SuGameDetailsResponse
+  const row = data.data?.regions?.[0]?.rows?.[0]?.cells ?? null
+
+  if (row) {
+    return {
+      homeTeamId: row[0].link.ids[0],
+      homeTeamLogo: row[0].image.url,
+      guestTeamId: row[2].link.ids[0],
+      guestTeamLogo: row[2].image.url
+    }
+  } else {
+    return null
+  }
+}
 
 async function fetchGamesBySeason(season: number): Promise<Game[]> {
   const url = 'https://api-v2.swissunihockey.ch/api/games?mode=team&season=' + season + '&team_id=429321&games_per_page=1000'
@@ -11,7 +33,7 @@ async function fetchGamesBySeason(season: number): Promise<Game[]> {
   const data = (await res.json()) as SuResponse
   const rows = data.data?.regions?.[0]?.rows ?? []
 
-  return rows.map((row) => {
+  const games = rows.map((row) => {
     const id = row.link.ids[0];
     const date = row.cells[0].text[0]
     const time = row.cells[0].text[1]
@@ -25,11 +47,34 @@ async function fetchGamesBySeason(season: number): Promise<Game[]> {
       id: id,
       date: `${date} ${time}`,
       location: `${location1}, ${location2}`,
-      homeTeam: homeTeam ?? 'Unknown',
-      guestTeam: guestTeam ?? 'Unknown',
+      homeTeam: homeTeam ?? '?',
+      homeTeamId: 0,
+      homeTeamLogo: '',
+      guestTeam: guestTeam ?? '?',
+      guestTeamId: 0,
+      guestTeamLogo: '',
       result: result ?? undefined
     }
   })
+
+  return Promise.all(games.map(async (game) => {
+    const details = await fetchGame(game.id)
+    if (details) {
+      return {
+        id: game.id,
+        date: game.date,
+        location: game.location,
+        homeTeam: game.homeTeam,
+        homeTeamId: details.homeTeamId,
+        homeTeamLogo: details.homeTeamLogo,
+        guestTeam: game.guestTeam,
+        guestTeamId: details.guestTeamId,
+        guestTeamLogo: details.guestTeamLogo,
+        result: game.result
+      }
+    }
+    return game
+  }))
 }
 
 export const handler: Handler = async () => {
--- shared/types.ts
+++ shared/types.ts
@@ -17,16 +17,48 @@ export interface SuResponse {
   }
 }
 
+export interface SuGameDetailsRow {
+  cells: Array<{
+    image: {
+      url: string
+    },
+    text: string[],
+    link: {
+      ids: number[]
+    }
+  }>
+
+}
+
+export interface SuGameDetailsResponse {
+  data: {
+    regions: Array<{
+      rows: SuGameDetailsRow[]
+    }>
+  }
+}
+
 export interface Game {
   id: number,
   date: string
   location: string
   homeTeam: string
+  homeTeamId: number
+  homeTeamLogo: string
   guestTeam: string
+  guestTeamId: number
+  guestTeamLogo: string
   result?: string
 }
 
 export interface GamesOverview {
   past: Game[],
   future: Game[]
+}
+
+export interface GameDetails {
+  homeTeamId: number
+  homeTeamLogo: string
+  guestTeamId: number
+  guestTeamLogo: string
 }
